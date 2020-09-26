fd7d82b02eee853c07dee342b0fc19255a55337c
Angelica Celestino <angelicacelestino@Angelicas-MacBook-Pro.local>
2020-09-26 18:18:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/userregistration/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.userregistration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.userregistration.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //the "continue" button on page 1 will take us to registration page
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+
+    }
+
+    public void openActivity2() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
